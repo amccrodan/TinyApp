4859db1a5a1a154c8502fc78cd66e8f40be9d39a
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-17 21:30:21 +0000

    Added routing for urls/new and the post method for /urls with default behaviour.

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 var urlDatabase = {
@@ -24,6 +27,15 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get('/urls/:id', (req, res) => {
