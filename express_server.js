7c55f23981735e77f3ca47cbf2f4cc09bf52e45e
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-18 18:44:57 +0000

    Changed some vars to consts to use ES6 conventions. Updated root route to redirect to /urls.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 
-var urlDatabase = {
+const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
@@ -28,7 +28,7 @@ function generateRandomString(length) {
 // Routing info
 
 app.get('/', (req, res) => {
-  res.end('Hello!');
+  res.redirect('/urls');
 });
 
 app.get('/urls.json', (req, res) => {
